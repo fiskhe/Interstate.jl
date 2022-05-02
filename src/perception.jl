09acd9769c90d328bc74f403a43f92d30fc0de4c
Reena Zhang <reena.zhang22@gmail.com>
2022-05-02 00:12:59 -0500

    it is supposed to work but I doubt it

--- src/perception.jl
+++ src/perception.jl
@@ -9,6 +9,23 @@ struct ObjectState
     height::Float64
 end
 
+struct ExtendedObjectState
+    x::Float64
+    y::Float64
+    θ::Float64
+    length::Float64
+    width::Float64
+    height::Float64
+    velocity::Float64
+    ang_velocity::Float64
+end
+
+struct KalmanState
+    x_k::ExtendedObjectState
+    P_k::Matrix{Float64}
+    meas::Vector{Float64}
+end
+
 function position(o::ObjectState)
     [o.x, o.y]
 end
@@ -33,10 +50,29 @@ end
 function object_tracker(SENSE::Channel, TRACKS::Channel, EMG::Channel, camera_array, road)
     lines = []
 
+    # kalman filter initialization
+    c1 = camera_array[1]
+    c2 = camera_array[2]
+    kalman_states = kalman_init(SENSE[1], c1)
+
     while true
         sleep(0)
         @return_if_told(EMG)
         meas = @fetch_or_continue(SENSE)
+
+        c1_meas = SENSE[1]
+        c2_meas = SENSE[2]
+
+        for state in kalman_states
+            c1_match = match_bb_to_track(state, c1_meas, camera_array[1])
+            c2_match = match_bb_to_track(state, c2_meas, camera_array[2])
+            state.meas = cat(c1_meas[c1_match][1:4], c2_meas[c2_match][1:4])
+            delete!(c1_meas, c1_match)
+            delete!(c2_meas, c2_match)
+            kalman_filter(state)
+        end
+
+        #TODO: create funct to add one kalman state
         
         #tracks = TracksMessage(...)
         #TODO your code here
@@ -44,7 +80,24 @@ function object_tracker(SENSE::Channel, TRACKS::Channel, EMG::Channel, camera_ar
     end
 end
 
-# initialize values for first iteration of Kalman filter
+function kalman_filter(prev_kalman_state)
+   
+    x_prev = prev_kalman_state.x_k
+    P_prev = prev_kalman_state.P_k
+
+    x_k_forecast = obj_state_forecast(x_prev, 0.1) # 0.1 time step for now
+    P_k_forecast = p_forecast(x_prev, P_prev)
+    Kalman = kalman_gain(P_k_forecast, x_k_forecast)
+    P_k = p_k(Kalman, x_k_forecast, P_k_forecast)
+    x_k = obj_state_next(x_k_forecast, kalman, prev_kalman_state.meas)
+
+    # TODO: check if this actually works!!!!!
+    prev_kalman_state.x_k = x_k
+    prev_kalman_state.P_k = P_k
+
+end
+
+# initialize values for first iteration of Kalman filter with camera1
 function kalman_init(bb_c1, c1; loop_radius=50.0)
     # c1=camera_array[1]
     f = c1.focal_len
@@ -55,13 +108,10 @@ function kalman_init(bb_c1, c1; loop_radius=50.0)
     camera_pos = SVector{3,Float64}(5.0/6*loop_radius, loop_radius, 20.0)
     lookat = SVector{3,Float64}(0, 0, 0)
 
+    # initialize vector of all current tracks
+    kalman_states = Vector{KalmanState}()
 
-    #bb_c1 = bb_sense[1]
-    obj_estimates = [] # global frame 3d points representing center of each bb in c1
-
-    for bbox in bb_c1
-        println(bbox)
-        println()
+    for (i, bbox) in enumerate(bb_c1)
         tl = [bbox.left bbox.top]
         br = [bbox.right bbox.bottom]
         center_pix = midpoint(tl, br)
@@ -69,14 +119,8 @@ function kalman_init(bb_c1, c1; loop_radius=50.0)
         center_cf[3] = euclidean_dist(camera_pos, lookat)
         center_cf[1] = center_pix[1] * (sx/f) * center_cf[3]
         center_cf[2] = center_pix[2] * (sy/f) * center_cf[3]
-        println(center_cf)
-        println()
-        println(t)
-        println()
 
         center_gf = inv(R)*(center_cf - t)
-        println(center_gf)
-        println()
         x = center_gf[1]
         y = center_gf[2]
         extra_size = rand()
@@ -86,82 +130,25 @@ function kalman_init(bb_c1, c1; loop_radius=50.0)
         v = 5.0 + rand()*5.0
         θ = rand() * 2.0 * pi - pi
         angular_vel = v / loop_radius
-        push!(obj_estimates, [x y θ l w h v angular_vel])
+        x_0 = ExtendedObjectState(x, y, θ, l, w, h, v, angular_vel)
+        variances = [25, 25, 25, 25, 4, 1, 25, 0.04] # all overestimations
+        P_0 = diagm(variances)
+
+        curr_state = KalmanState(x_0, P_0, [bbox.left bbox.top bbox.right bbox.bottom])
+        push!(kalman_states, curr_state)
     end
 
-    println(obj_estimates)
-    println()
-    println()
-
-    obj_estimates
+    kalman_states
 end
 
-function match_bb(bb_cf, camera_array)
-    # transform input bb in global frame
-    # match by euclidean distance, output stacked Zk
-    # BEFORE: transform everything into global reference frame points
-        # x3* = 1.0 for all... thus, x1* = y1/f, x2* = y2/f
-        # [x1*, x2*, x3*] --> into [x1, x2, x3] --> create bounding boxes assuming f = 1 
-        # y1 = x1/x3, y2 = x2/x3 --> compare these below
-    # RETURN the transformed global bounding boxes
-    # 1. calculate euclidean distance between TL and BR, add them
-    # 2. put into matrix (n x n) where a12 is the distance between the first and second bb
-    # note: this is dependent on whether we know which camera is associated with which bb
-    # 3. find minimum in each row and col, if multiple match with same box, give priority to the lowest match
-    # 4. 
-
-    # get global points of bounding boxes
-    c1_bb_global = []
-    c2_bb_global = []
-    for i in 1:length(bb_cf[1])
-        bb1 = camera_to_global(camera_array[1], bb_cf[1][i])
-        push!(bb1, c1_bb_global)
+function match_bb_to_track(kalman_state, bb_list, camera)
+    kalman_bbox = h_state_to_bbox(kalman_state.x_k, camera)
+    euclid_dists = []
+    for (i, bb) in enumerate(bb_list)
+        push!(euclid_dists, sqrt(kalman_bbox[1]-bb_list[1])^2 + (kalman_bbox[2]-bb_list[2]^2))
     end
-    for i in 1:length(bb_cf[2])
-        bb2 = camera_to_global(camera_array[2], bb_cf[2][i])
-        push!(bb2, c2_bb_global)
-    end
-
-    # construct matrix of (euclidean distances)^2
-    # stores sum of TL and BR euclidean dist between ith meas from c1 and jth meas from c2
-    row = length(c1_bb_global)
-    col = length(c2_bb_global)
-    dist_matrix = zeros(Float64, row, col)
-    min_row = [] # keep (smallest dist, ind) in a row
-    min_col = [] # keep (smallest dist, ind) in a col
-    for (i, c1_bb) in enumerate(c1_bb_global)
-        for (j, c2_bb) in enumerate(c2_bb_global)
-            p1_tl = c1_bb[1:2]
-            p1_br = c1_bb[3:4]
-            p2_tl = c2_bb[1:2]
-            p2_br = c2_bb[3:4]
-            p1_midpoint = midpoint(p1_tl, p1_br)
-            p2_midpoint = midpoint(p2_tl, p2_br)
-            sum_dist = euclidean_dist_sq(p1_midpoint, p2_midpoint)
-            dist_matrix[i, j] = sum_dist
-        end
-        # findmin gives (value, index) of min element; index is corresponding bb match
-        push!(min_row, findmin(dist_matrix[i]))
-    end
-    for j in 1:length(c2_bb_global)
-        push!(min_col, findmin(dist_matrix[:,j]))
-    end
-
-    # add match if both row and col found each other as min
-    matches = []
-    bb_final = []
-    last_ind = max(row, col)
-    for i in 1:last_ind
-        row_match = min_row[i][2]
-        if min_col[row_match][2] == i 
-            push!(matches, [i, row_match])
-            c1_bb = bb_cf[1][i]
-            c2_bb = bb_cf[2][row_match]
-            push!(bb_final, [c1_bb c2_bb])
-        end
-    end
-
-    bb_final
+    best_match = findmin(euclid_dists)
+    return best_match[2] #index of best match bb
 end
 
 # converts bounding boxes from camera frame to global frame 2d pixels (assumes f=1)
